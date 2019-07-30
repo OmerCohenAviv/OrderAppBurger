807a307fdaa96bf5f96d6c92697658f406b7ebbf
Pokernab <53443894+Pokernab@users.noreply.github.com>
2019-07-30 20:45:40 +0300

    Branch

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -33,7 +33,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
                     <Modal 
                     show={this.state.error} 
                     modalClosed={this.closeModalHandler}> {this.state.error ? this.state.error.message : null } </Modal>
-                    <WrappedComponent {...this.props} />
+                    <WrappedComponent  {...this.props} />
                 </Fragment>
             );
         };
